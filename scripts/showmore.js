0357e4a41565dd44f1d0c17d900f0e0eee248e50
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-08-08 12:49:36 +0000

    start

--- scripts/showmore.js
+++ scripts/showmore.js
@@ -12,7 +12,7 @@ export function initShowMore() {
 
 				[...blockChilds].forEach((item, index) => {
 					if (index <= (step - 1)) {
-						item.classList.toggle('show');
+						item.classList.add('show');
 					}
 				})
 			})
@@ -21,13 +21,28 @@ export function initShowMore() {
 	function initButtons() {
 		if (showMoreButtons.length) {
 			[...showMoreButtons].forEach(button => {
+				const noMore = button.querySelector('[data-no-more]');
+				const more = button.querySelector('[data-more]');
+
+				if (!button.classList.contains('active')) {
+					more.style.display = 'inline';
+					noMore.style.display = 'none';
+				}
 				button.addEventListener('click', e => {
 					e.preventDefault();
 					button.classList.toggle('active');
 					showAction();
+
+					if (button.classList.contains('active')) {
+						more.style.display = 'none';
+						noMore.style.display = 'inline';
+				} else {
+						more.style.display = 'inline';
+						noMore.style.display = 'none';
+				}
 				});
 			});
-  	}
+    }
 	}
 	function showAction() {
 		if (showMoreContents.length) {
@@ -36,7 +51,7 @@ export function initShowMore() {
 				const blockChilds = [...blockContent.children];
 				if (blockChilds.length) {
 					blockChilds.forEach((child, index) => {
-						if (index > (step - 1)) {
+						if (index >= (step)) {
 							child.classList.toggle('show');
 						}
 					})
