361f24e598441d4de417e64f689b25dca3237832
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-08-08 10:13:52 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ import { initSlider } from './slider.js';
 import { initNavigation } from './menu.js';
 import { initForms } from './forms.js';
 import { initHeader } from './header.js';
+import { initShowMore } from './showmore.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,12 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
   initSlider();
   initNavigation();
   initForms();
-
-
-  // watcherToggle('.hero .scroll-anim', 0.3);
-  // watcherAnim('[data-counter]', 0.2, countAnimate, false);
-  // watcherToggle('.hero .scroll-anim', 0.3);
-  // watcherToggle('.section-header.scroll-anim', 0.9);
-  // watcherToggle('.map .scroll-anim', 0.3);
-  // watcherToggle('.get-info .scroll-anim', 0.3);
+  initShowMore();
 });
--- scripts/showmore.js
+++ scripts/showmore.js
@@ -1,57 +1,50 @@
 'use strict';
 
-import { setCSSProperty } from "./functions.js";
-
-
-
 export function initShowMore() {
-	const showMoreBlocks = document.querySelectorAll('[data-showmore]');
-
-	if (showMoreBlocks.length) {
-		showMoreBlocks.forEach(block => {
-			const showMoreContent = block.querySelector('[data-showmore-content]');
-			const step = showMoreContent.dataset.showmoreContent;
-			const type = block.dataset.showmore;
-
-				if(type == 'items') {
-					const items = block.querySelectorAll('li');
-					items.forEach((item, index) => {
-						if (index > (parseFloat(step) - 1)) {
-							setCSSProperty(item, '--height', '0');
-							setCSSProperty(item, '--overflow', 'hidden');
-				}
-		})
-	}
-		})
-	}
 	const showMoreButtons = document.querySelectorAll('[data-showmore-button]');
+	const showMoreContents = document.querySelectorAll('[data-showmore-content]');
 
-	if (showMoreButtons.length) {
-		[...showMoreButtons].forEach(button => {
-			button.addEventListener('click', e => {
-				e.preventDefault();
-				showMoreActions(e);
+	function initShowItems() {
+		if (showMoreContents.length) {
+			showMoreContents.forEach(block => {
+				const step = parseFloat(block.dataset.showmoreContent);
+				const blockChilds = block.children;
+
+				[...blockChilds].forEach((item, index) => {
+					if (index <= (step - 1)) {
+						item.classList.toggle('show');
+					}
+				})
 			})
-		})
-  }
-
-	function showMoreActions(e) {
-		const showMoreButton = e.target.closest('[data-showmore-button]');
-		const showMoreBlock = showMoreButton.closest('[data-showmore]');
-		const showMoreContent = showMoreBlock.querySelector('[data-showmore-content]');
-		const type = showMoreBlock.dataset.showmore;
-		const step = showMoreContent.dataset.showmoreContent;
-		showContent(type, step, showMoreContent);
-}
-function showContent(type, step, block) {
-	if(type == 'items') {
-		const items = block.querySelectorAll('li');
-		items.forEach((item, index) => {
-			if (index > (parseFloat(step) - 1)) {
-				setCSSProperty(item, '--height', 'auto');
-				setCSSProperty(item, 'overflow', 'auto');
-			}
-		})
+		}
 	}
+	function initButtons() {
+		if (showMoreButtons.length) {
+			[...showMoreButtons].forEach(button => {
+				button.addEventListener('click', e => {
+					e.preventDefault();
+					button.classList.toggle('active');
+					showAction();
+				});
+			});
+  	}
+	}
+	function showAction() {
+		if (showMoreContents.length) {
+			showMoreContents.forEach(blockContent => {
+				const step = parseFloat(blockContent.dataset.showmoreContent);
+				const blockChilds = [...blockContent.children];
+				if (blockChilds.length) {
+					blockChilds.forEach((child, index) => {
+						if (index > (step - 1)) {
+							child.classList.toggle('show');
+						}
+					})
+				}
+			});
+		}
+	}
+	initShowItems();
+	initButtons();
 }
-}
+
